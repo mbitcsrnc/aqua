a85d0ecebec6e762e1c7834e40708b6243af5ef7
mbitcsrnc <137778239+mbitcsrnc@users.noreply.github.com>
2025-08-07 11:32:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html>
+<title>AquaMinz RO The best water Purifier Brand in India</title><head>
+<meta charset="utf-8">
+  <meta name="description" content="AquaMinz RO India&rsquo;s 1st alkaline water purifier company providing the alkaline RO purifiers for home and industrial / commercial use. It also provide water softener, water cooler, Iron remover, UV purifier, UF purifiers and gravity water Purifier."/>
+  
+<meta name="keywords" content="AquaMinz, AquaMinzRO,AquaMinzro, Water Purifiers, Water Softener, Water Purifier Price, home Water Filter, RO Water Purifier, Best Water Purifiers in India, Water Purifier Online, water Filters, UV Purifiers, UV Water Purifier, Gravity water Purifiers, RO Spare Parts, alkaline ro, 100 LPH RO,Water Dispenser, Water Cooler,Hot RO, Cold RO, Hot and Cold RO"/>
+<meta name="author" content="Magic Bubbles IT Consultancy Service">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=K2D|Ubuntu" rel="stylesheet"> 
+ <?php include 'mhhd.php';?>
+ <style>
+ body, html {
+    height: 100%;
+	background-color: #fcfcfd;
+}
+.bdy{
+	margin:25px 5px 5px 5px;
+	
+}
+.bdycl{
+	margin:1px 12px 1px 12px;
+	padding-bottom:5px;
+	width: 250px;
+	height: 200px;
+	border: 1px solid #dbdbdb;
+	padding: 20px;
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+	-webkit-box-shadow:  0px 0px 5px 0px #dddddd;
+	box-shadow:  0px 0px 5px 0px #dddddd;
+	background-color: #FBFBFB;
+	background-image:url("image/image/bgwtr.jpg");
+	background-repeat:no-repeat;
+	float:left;
+} 
+.bdycl1{
+	
+	margin-bottom:50px;
+	float:left;
+}
+.a{
+	text-align: center;
+}
+hr{
+	width:80%;
+	border: 0; height: 1px; 
+	background-image: linear-gradient(to right, rgba(0, 0, 255, 0), rgba(0, 0, 255, 0.75), rgba(0, 0, 255, 0));
+}
+#myBtn {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  background-color: red;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 4px;
+}
+
+#myBtn:hover {
+  background-color: #555;
+}	
+.x1{
+
+background:linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)),url("image/image/bg1.png");
+background-repeat:repeat;
+width:100%;
+min-width:500px;;
+text-align:center;
+
+margin:0px;
+}
+ul.x1_1 {
+    list-style-type: none;
+   
+    padding:50px;
+    overflow: hidden;
+margin-left:25%;
+
+}
+li.x1_1 {
+    float: left;
+	
+}
+img.xx1{
+	width:auto;
+	height:200px;
+}
+h4.kk1{
+	font-family: 'K2D', sans-serif;
+	font-size:14px;
+	font-weight:500;
+	color:#006633;
+	
+	float:left;
+	padding:1px 22px;
+}
+p.hh1{
+	display:inline-block;
+	text-align:justify;
+	font-family:'Ubuntu', sans-serif;
+	padding:1px 22px;
+}
+.container{
+	padding:0px;
+	margin:0px;
+}
+.pp{
+	padding:10px 0px 0px 200px;
+}
+@media only screen and (max-width: 700px){
+ul.x1_1 {
+    list-style-type: none;
+   
+    padding:15px;
+    overflow: hidden;
+margin-left:5%;
+
+}
+
+img.xx1{
+	width:auto;
+	height:80px;
+}
+h3.roco{
+	font-size:12px;
+}
+.pp{
+	padding:0px;
+}
+
+}
+ </style>
+
+</head>
+<body>
+
+
+<?php include 'mnu.php';?>
+<?php /*include 'popup.php';*/?>
+<?php include 'himcar.php';?>
+
+<button onClick="topFunction()" id="myBtn" title="Go to top"><span class="glyphicon glyphicon-circle-arrow-up"></span> </button>
+
+<div class="container-fluid">
+
+<div class="row bdy">
+	<!--
+	<div class="a">
+		<button type="button" class="btn btn-success" style="width:250px;height:auto" onClick="" >Request A Free Demo</button>
+		<hr>
+	</div>
+	-->
+
+	
+			<div class="col-sm-3">
+				<div class="bdycl">
+					<!--<img src="image/image/somya1.png" height="180px" width="auto" >-->
+					<img src="image/image/tsop.png" height="80px" width="auto" >
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="bdycl">
+						<img  src="image/image/simple.png" height="180px" width="auto"  >
+				</div>
+				<h4 class="kk1">Gravity based Purifiers</h4>
+				<p class="hh1">Gravity based purifiers are very suitable for home environment<br><a href="#">Read more...</a></p>
+				
+			</div>
+			<div class="col-sm-3">
+					<div class="bdycl">
+						<img  src="image/image/domestic.png" height="94px" width="auto">
+						
+					</div>
+				<h4 class="kk1">RO+UV+UF+TDS+ALKALINE Purifiers</h4>
+				<p class="hh1">Reverse Osmasis Techonogy based purifiers are very suitable for home environment<br><a href="#">Read more...</a></p>
+			</div>			
+			
+			<div class="col-sm-3">
+					<div class="bdycl">
+						<img  src="image/image/comm.png" height="94px" width="auto">
+					</div>
+				<h4 class="kk1">RO+UV+UF+TDS+ALKALINE Commercial Purifiers</h4>
+				<p class="hh1">Reverse Osmasis Techonogy based commercial purifiers are very suitable for heavy duty commercial and big company environment<br><a href="#">Read more...</a></p>
+					
+			</div>
+			
+
+</div><!--row-->
+</div>
+<div class="container x1"> <!-- new container-->
+	<h3 style="color:#0033FF">AquaMinz RO Purifiers</h3>
+	<hr>
+		<ul class="x1_1">
+			<li class="x1_1"><img class="xx1" src="image/prod/gentleplus.png"></li>
+			<li class="x1_1"><img class="xx1" src="image/prod/gloryplus.png"></li>
+			<li class="x1_1"><img class="xx1" src="image/prod/misty.png"></li>
+		</ul>
+	</div><!--new row-->
+	<!--
+	<div class="row">
+		<div class="col-sm-4">
+			<img class="xx1" src="image/prod/gentleplus.png">
+		</div>
+		<div class="col-sm-4">
+			<img class="xx1" src="image/prod/gloryplus.png">
+		</div>
+		<div class="col-sm-4">
+			<img class="xx1" src="image/prod/misty.png">
+		</div>
+
+	</div>
+	-->
+</div>
+<div class="container pp" >
+	<?php include 'indbdy.php';?>
+</div>
+<?php include 'footer.php';?>
+
+<script>
+	window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
+
+<script>
+function goBack() {
+    window.history.back();
+}
+</script>
+
+</body>
+</html>
